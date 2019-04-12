529e8128ba31a68645023be25d54c253d69706cc
Miguel Velez <miguelvelez@mijecu25.com>
2019-04-12 09:09:54 -0400

    added utility class to transform from bdd to sat feature expr

---
+++ src/main/java/edu/cmu/cs/mvelezce/parser/sat/SATFeatureExprParser.java
@@ -0,0 +1,27 @@
+package edu.cmu.cs.mvelezce.parser.sat;
+
+import de.fosd.typechef.featureexpr.FeatureExpr;
+import de.fosd.typechef.featureexpr.FeatureExprFactory;
+import de.fosd.typechef.featureexpr.bdd.BDDFeatureExpr;
+import java.util.ArrayList;
+import java.util.List;
+
+// Noninstantiable utility class
+public class SATFeatureExprParser {
+
+  // Suppress default constructor for noninstantiability
+  private SATFeatureExprParser() {}
+
+  public static List<FeatureExpr> toSatFeatureExprs(List<FeatureExpr> featureExprs) {
+    List<FeatureExpr> satFeatureExprs = new ArrayList<>();
+
+    for (FeatureExpr featureExpr : featureExprs) {
+      FeatureExpr satFeatureExpr = ((BDDFeatureExpr) featureExpr).toSATFeatureExpr();
+      satFeatureExprs.add(satFeatureExpr);
+    }
+
+    FeatureExprFactory.setDefault(FeatureExprFactory.sat());
+
+    return satFeatureExprs;
+  }
+}
